06910dc72f7968297d78d73c40e3420576ff9018
tjm67 <tjm67@njit.edu>
2020-12-07 02:49:21 -0500

    Very, very basic.

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+source = requests.get('https://web.njit.edu/~tjm67/parsetest.html').text
 
+soup = BeautifulSoup(source, 'lxml')
+
+for article in soup.find_all('div', class_='article'):
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.p.text
+    print(summary)
+    print()
